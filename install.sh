6ff335acca0d0d4247186a068a8a3ae48f6861f6
dyc5828 <dyc5828@gmail.com>
2025-11-27 21:06:04 -0800

    fixed install script and disable vscode ruby lsp

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
   else
     echo "Backing up pre-existing dot files.";
-    dot config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 dot checkout
-dot config status.showUntrackedFiles no
+dot status.showUntrackedFiles no
